7ce1086ea17c03630ffac9729f6f24f04851d4ce
shh <heun3316@naver.com>
2023-03-26 11:03:47 +0900

    java v03

---
+++ src/Operator.java
@@ -0,0 +1,32 @@
+public class Operator {
+    public static void main(String[] args) {
+//        int number = 5;
+//        number = (int) 1.2;
+//        System.out.println(number);
+//
+//        System.out.println(1 + 1.2);
+//        System.out.println(7 / 2);
+//        System.out.println(7 / 2.0);
+//
+//        System.out.println(2 < 3);
+//        System.out.println(2 <= 3);
+//        System.out.println(2 > 3);
+//        System.out.println(3 >= 3);
+//        System.out.println(5 == 3);
+//        System.out.println(5 != 5);
+//        System.out.println(3 != 5);
+
+        //예시 문제
+        int x = 5;
+        int y = 2;
+        System.out.println(x < y); //false
+        System.out.println(y > x); //false
+        System.out.println(y <= x); //true
+        System.out.println(y * y); //4
+        System.out.println(x * y); //10
+        System.out.println(y % x); //0
+        System.out.println(y % 2); //0
+        System.out.println(x == 5); //true
+        System.out.println(2 != y); //false
+    }
+}
