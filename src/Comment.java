b9b06f32dc8c9400e8c732a4020721c723807320
shh <heun3316@naver.com>
2023-03-26 09:53:15 +0900

    java v01

---
+++ src/Comment.java
@@ -0,0 +1,6 @@
+public class Comment {
+    public static void main(String[] args) {
+        //나는 주석입니다.
+        System.out.println("주석을 사용해 보세요.");
+    }
+}
---
+++ src/StringExample.java
@@ -0,0 +1,9 @@
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("Im String");
+        System.out.println('나');
+        System.out.println("안녕 나는 \"자바\" 야");
+        System.out.println("치킨은 살 안쪄요. \n내가 살 쪄요.");
+        //나는 주석입니다.
+    }
+}
