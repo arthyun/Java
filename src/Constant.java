edbdccdf616ce19e4e49eb90552e45d169605dfa
arthyun <heun3316@naver.com>
2023-03-27 10:16:01 +0900

    java v03.5

---
+++ src/Constant.java
@@ -0,0 +1,13 @@
+public class Constant {
+    public static void main(String[] args) {
+        final int num1 = 100;
+        //num1 = 1000;
+        System.out.println(num1);
+        System.out.printf("열이 %d도 입니다\n", num1);
+
+        float num2 = 13;
+        double num3 = 15;
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
