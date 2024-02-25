4e057f15bd37c77cca054b060a2c95f23178e19a
arthyun <heun3316@naver.com>
2024-02-25 21:26:24 +0900

    v4

--- springBoot/src/main/java/practice/springBoot/controller/HelloController.java
+++ springBoot/src/main/java/practice/springBoot/controller/HelloController.java
@@ -3,6 +3,7 @@ package practice.springBoot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
@@ -12,5 +13,11 @@ public class HelloController {
     public String hello (Model model){
         model.addAttribute("data", "SpringBoot!!"); // { data : "SpringBoot!!" }로 할당
         return "hello"; // templates 폴더 하위 html명을 return값 기준으로 찾음
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-mvc";
     }
 }
