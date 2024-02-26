759736d86459f42cadecf4d7fcdb05d679a4adcc
heun3316 <heun3316@naver.com>
2024-02-26 09:38:39 +0900

    v5_get json api

--- springBoot/src/main/java/practice/springBoot/controller/HelloController.java
+++ springBoot/src/main/java/practice/springBoot/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
@@ -19,5 +20,33 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // 입력한 param에 따라 body값을 내려줌
+    }
+
+    /* 실제 사용 방식 */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); // 아래에서 생성한 클래스로 인스턴스를 만듬
+        hello.setName(name);
+        return hello;
+    }
+
+    // getter/setter 클래스 생성 (json을 만들어줌~)
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
