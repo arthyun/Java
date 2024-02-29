d26d8d87bf492e2ac0fc2222369d3aced3bfcf2b
heun3316 <heun3316@naver.com>
2024-02-29 17:44:06 +0900

    v19

--- mybatis/src/main/java/com/springboot/mybatis/main/controller/MainController.java
+++ mybatis/src/main/java/com/springboot/mybatis/main/controller/MainController.java
@@ -2,10 +2,13 @@ package com.springboot.mybatis.main.controller;
 
 import com.springboot.mybatis.main.dto.MainDto;
 import com.springboot.mybatis.main.service.MainService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.EOFException;
 import java.util.List;
 
 @RestController
@@ -18,8 +21,13 @@ public class MainController {
 
     @GetMapping("/main")
     public List<MainDto> getUsers () {
-
-        List<MainDto> userList = mainService.getUserInfo();
+        List<MainDto> userList = null;
+        try {
+            userList = mainService.getUserInfo();
+        } catch (Exception e){
+            e.printStackTrace();
+            e.getMessage();
+        }
         return userList;
     }
 }
