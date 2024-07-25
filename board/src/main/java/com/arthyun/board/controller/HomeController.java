2e9ccd7c49035ba68d480c4e638fa78b7b85d1d3
arthyun <heun3316@naver.com>
2024-07-25 14:48:19 +0900

    add board project

---
+++ board/src/main/java/com/arthyun/board/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.arthyun.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        System.out.println("HomeController.index");
+        return "index";
+    }
+}
